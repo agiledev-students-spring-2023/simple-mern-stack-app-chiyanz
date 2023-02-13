05f90581f604522a80f521c4ae6f74bf64628924
Johnny Zhou <jonathanqyz@gmail.com>
2023-02-13 16:53:20 -0500

    implemented about us page and ngrok deploy

---
+++ front-end/src/About.js
@@ -0,0 +1,20 @@
+import pfp from './Headshot.jpg'
+
+const About = () => {
+  return (
+    <>
+      <img src={pfp} width="450"></img>
+      <p>
+          I'm a very ambitious front-end developer who is looking for an opporunity to learn more about latest technology and to challenge myself through diverse and interesting projects. I have 8 years of background in programming, have a solid foundation in object-oriented programming with Java, then built on my knowledge with Python, C++, and most recently JavaScript. 
+      </p>
+      <p>
+          I'm passion driven, detail oriented, and continuously working on honing my skills towards the goals I've dedicated myself to. My club involvement and team leading experience makes me a team player and capable of organization as well as management. I've taken on the role of lead project manager for multiple Hackthons and led my team to win multiple prizes. I have a strong desire to innovate, to build applications that can benefit users and create lasting impact. 
+      </p>
+      <p>
+          If I were to define myself in one sentence, I would describe myself as meticulous, responsible, someone who is really enthusiastic about his hobbies, a curious soul with passion towards my career, and a natural perfectionist.
+      </p>
+    </>
+  )
+}
+
+export default About
